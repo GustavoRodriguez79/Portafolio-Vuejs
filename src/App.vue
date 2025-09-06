e6ca04e289ff3b1028ec91b6dbe205ad2c204d79
Gustavo Rodriguez <garodrifornes79@gmail.com>
2025-09-06 17:27:33 -0300

    Agrego la capítulo 2 del portafolio en Vuejs

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const nombreApellido = 'Gustavo Ariel Rodriguez';
+
+</script>
+
+
+<template>
+  <header>
+    <h1>{{ nombreApellido }} </h1>
+    <p> Bienvenido a Vuejs</p>
+  </header>
+
+</template>
+
+<style scoped>
+
+
+</style>
